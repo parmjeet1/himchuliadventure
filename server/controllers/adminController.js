05de6a5061ec555f943bdd95879d4974cb50149e
parmjeet1 <paramjeet>
2025-02-28 14:51:26 +0530

    login id

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -41,7 +41,7 @@ const adminLogin = async (req, res) => {
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
 
-    return res.status(200).json({"message":"login successfully","token":token})
+    return res.status(200).json({"message":"login successfully!","token":token, userId:user._id})
 
     }catch(error){
        return res.status(400).json({"internal error":error});
