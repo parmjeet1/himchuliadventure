a2d05719f5c4bbb7146db2cae15de337d7b74e14
parmjeet1 <paramjeet>
2025-03-02 15:01:39 +0530

    fetch customer details

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -5,7 +5,7 @@ const addCustomerDetail = async (req, res) => {
         console.log("dfsdsd");
         const { packageId, name, email, mobile, message } = req.body;
 
-        // Create customer data object dynamically
+        
         const customerData = { name, email, mobile, message };
         if (packageId) {
             customerData.packageId = packageId;
@@ -27,4 +27,21 @@ const addCustomerDetail = async (req, res) => {
     }
 };
 
-module.exports = { addCustomerDetail };
+const fetchCustomerDetails = async (req, res) => {
+    try {
+        const customers = await customerModel.find().sort({ createdAt: -1 }).lean();
+
+        if (!customers || customers.length === 0) {
+            return res.status(404).json({ error: "No customers found" });
+        }
+
+        return res.status(200).json({
+            status: "success",
+            data: customers
+        });
+
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error", details: error.message });
+    }
+};
+module.exports = { addCustomerDetail,fetchCustomerDetails };
--- server/routes/customerRoute.js
+++ server/routes/customerRoute.js
@@ -1,5 +1,8 @@
 const express=require("express");
-const { addCustomerDetail } = require("../controllers/contactController");
+const { addCustomerDetail, fetchCustomerDetails } = require("../controllers/contactController");
 const customerRouter=express.Router();
 customerRouter.post("/add-customer",addCustomerDetail);
+
+customerRouter.get("/fetch",fetchCustomerDetails);
+
 module.exports=customerRouter;
